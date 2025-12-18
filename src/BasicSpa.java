e90f83bb3c34e893445446163eb74318d78ea960
Jennifer Göransson <jennifer.goransson@yh.nackademin.se>
2025-12-18 15:33:53 +0100

    Created the class FacialTreatment which is a decorator to the BasicSpa

--- src/BasicSpa.java
+++ src/BasicSpa.java
@@ -1,12 +1,12 @@
 public class BasicSpa implements SpaService{
     @Override
     public String getDescription() {
-        return "Basic Spa Package – access to pool, sauna, and relaxation area.";
+        return "Basic Spa Package – access to pool, sauna, and relaxation area. 4 hours";
     }
 
     @Override
     public double getPrice() {
-        return 500;
+        return 800;
     }
 }
 
---
+++ src/FacialTreatment.java
@@ -0,0 +1,13 @@
+public class FacialTreatment extends SpaServiceDecorator {
+    public FacialTreatment(SpaService spaService){
+        super(spaService);
+    }
+    @Override
+    public String getDescription() {
+        return spaService.getDescription() + ", Facial Treatment 60 min";
+    }
+    @Override
+    public double getPrice() {
+        return spaService.getPrice() + 400;
+    }
+}
