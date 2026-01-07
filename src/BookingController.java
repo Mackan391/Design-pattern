0aec305cfc437fff57a5e8ec101f9d375557dac6
Jennifer Göransson <jennifer.goransson@yh.nackademin.se>
2026-01-07 19:40:28 +0100

    Created the base of the BookingController - the controller of our MVC

--- src/BookingController.java
+++ src/BookingController.java
@@ -1,2 +1,9 @@
 public class BookingController {
+    private BookingManager manager;
+    private BookingView view;
+
+    public BookingController(BookingManager manager, BookingView view) {
+        this.manager = manager;
+        this.view = view;
+    }
 }
