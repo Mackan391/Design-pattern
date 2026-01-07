594ec3d7a626ba391107b737525192515fe80dbe
Jennifer Göransson <jennifer.goransson@yh.nackademin.se>
2026-01-07 19:42:33 +0100

    Created a method that will create a booking for a user

--- src/BookingController.java
+++ src/BookingController.java
@@ -1,3 +1,5 @@
+import SpaServices.SpaService;
+
 public class BookingController {
     private BookingManager manager;
     private BookingView view;
@@ -5,5 +7,11 @@ public class BookingController {
     public BookingController(BookingManager manager, BookingView view) {
         this.manager = manager;
         this.view = view;
+    }
+
+    public void createBooking(User user, SpaService service, String date){
+        Booking booking = new Booking(user, service, date);
+        manager.addBooking(booking);
+        view.showMessage("Booking created successfully!");
     }
 }
